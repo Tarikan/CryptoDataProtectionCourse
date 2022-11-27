40492393c7335c2a2f46457230dcd0b7ee75e794
Taras Ivanyshyn <v-Taras.Ivanyshyn@lionbridge.com>
2022-11-27 20:51:38 +0200

    Add lab6

---
+++ Lab6/GF256.cs
@@ -0,0 +1,39 @@
+﻿namespace Lab6;
+
+public class GF256
+{
+    public const int Poly = 0x11b;
+
+    public static byte Mul02(byte b)
+    {
+        var b7 = b >> 7;
+        return (byte)((b << 1) ^ (b7 * Poly));
+    }
+
+    public static byte Mul03(byte b)
+    {
+        return (byte)(Mul02(b) ^ b ^ 0);
+    }
+    
+    public static byte Mul(byte b0, byte b1)
+    {
+        var product = 0;
+        for (var i = 0; i < 8; i++)
+        {
+            product <<= 1;
+            if ((product & 0x100) != 0)
+            {
+                product ^= Poly;
+            }
+
+            if ((b0 & 0x80u) != 0)
+            {
+                product ^= b1;
+            }
+
+            b0 <<= 1;
+        }
+
+        return (byte) product;
+    }
+}
---
+++ Lab6/Program.cs
@@ -0,0 +1,9 @@
+﻿using Lab6;
+
+byte a = 0xD4;
+byte b = 0xBF;
+
+Console.WriteLine(GF256.Mul(a, 0x02).ToString("X2"));
+Console.WriteLine(GF256.Mul(b, 0x03).ToString("X2"));
+Console.WriteLine(GF256.Mul02(a).ToString("X2"));
+Console.WriteLine(GF256.Mul03(b).ToString("X2"));
